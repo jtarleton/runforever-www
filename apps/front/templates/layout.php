36b3f01a721e65cf87f8b3625848685d34218626
jtarleton <jamestarleton@gmail.com>
2015-06-10 16:16:45 -0400

    updates

--- apps/front/templates/layout.php
+++ apps/front/templates/layout.php
@@ -1,14 +1,61 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html lang="en">
   <head>
     <?php include_http_metas() ?>
     <?php include_metas() ?>
     <?php include_title() ?>
+
     <link rel="shortcut icon" href="/favicon.ico" />
+
     <?php include_stylesheets() ?>
     <?php include_javascripts() ?>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,600,700' rel='stylesheet' type='text/css'>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="favicon.png">
+    <title>About</title>
+    <link href="css/style.css" rel="stylesheet">
+    <script src="js/chart.js"></script>
+
+    <!--[if lt IE 9]><script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+
   </head>
+
   <body>
-    <?php echo $sf_content ?>
+  <div class="sticky">
+<?php require('top.html'); ?>
+
+
+
+    <!-- Contant -->
+<?php echo $sf_content; ?>
+    <div class="push"></div>
+  </div>
+    <!--FOOTER-->
+    <div class="footer">
+      <div class="container clearfix">
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2013 Your Name | Template design by Veselka for <a href="http://www.andreasviklund.com">andreasviklund.com</a> | Best hosted at <a href="http://www.svenskadomaner.se">www.svenskadomaner.se</a></p>
+      </div>
+    </div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="js/bootstrap.js"></script>
   </body>
 </html>
+
+
+
+
+
