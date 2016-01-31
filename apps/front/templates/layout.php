30efaf03e9334126ea40d6dfaa7da4332f766ab2
James Tarletongit config --global user.name James <jamestarleton@gmail.com>
2016-01-31 17:47:59 -0500

    update html

--- apps/front/templates/layout.php
+++ apps/front/templates/layout.php
@@ -6,11 +6,11 @@
 -->
 <html>
   <head>
-    <title>Run Forever</title>
+    <title>Run Forever</title
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-    <link rel="stylesheet" href="http://www.runforever.co/assets/css/main.css" />
+    <link rel="stylesheet" href="http://www.runforever.co/Hassets/css/main.css" />
     <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
     <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
       <?php include_http_metas() ?>
@@ -68,7 +68,7 @@
   <div style="background-color:salmon; padding:10px;"  class="flash_error"><?php echo $sf_user->getFlash('error') ?></div>
 <?php endif ?>
 
-            
+
     <!-- Contant -->
 <?php echo $sf_content; ?>
 
@@ -92,7 +92,7 @@
 
             <!-- Intro -->
               <section id="intro">
-                <a href="#" class="logo"><img src="images/logo.jpg" alt="" /></a>
+                <a href="#" class="logo"><img src="http://www.runforever.co/images/logo.jpg" alt="" /></a>
                 <header>
                   <h2>Future Imperfect</h2>
                   <p>Aliquet elit adipiscing nunc faucibus ac Vestibulum</p>
@@ -108,9 +108,9 @@
                       <header>
                         <h3><a href="#">Vitae sed condimentum</a></h3>
                         <time class="published" datetime="2015-10-20">October 20, 2015</time>
-                        <a href="#" class="author"><img src="images/avatar.jpg" alt="" /></a>
+                        <a href="#" class="author"><img src="http://www.runforever.co/images/avatar.jpg" alt="" /></a>
                       </header>
-                      <a href="#" class="image"><img src="images/pic04.jpg" alt="" /></a>
+                      <a href="#" class="image"><img src="http://www.runforever.co/images/pic04.jpg" alt="" /></a>
                     </article>
 
                   <!-- Mini Post -->
@@ -118,9 +118,9 @@
                       <header>
                         <h3><a href="#">Rutrum neque accumsan</a></h3>
                         <time class="published" datetime="2015-10-19">October 19, 2015</time>
-                        <a href="#" class="author"><img src="images/avatar.jpg" alt="" /></a>
+                        <a href="#" class="author"><img src="http://www.runforever.co/images/avatar.jpg" alt="" /></a>
                       </header>
-                      <a href="#" class="image"><img src="images/pic05.jpg" alt="" /></a>
+                      <a href="#" class="image"><img src="http://www.runforever.co/images/pic05.jpg" alt="" /></a>
                     </article>
 
                   <!-- Mini Post -->
@@ -128,9 +128,9 @@
                       <header>
                         <h3><a href="#">Odio congue mattis</a></h3>
                         <time class="published" datetime="2015-10-18">October 18, 2015</time>
-                        <a href="#" class="author"><img src="images/avatar.jpg" alt="" /></a>
+                        <a href="#" class="author"><img src="http://www.runforever.co/images/avatar.jpg" alt="" /></a>
                       </header>
-                      <a href="#" class="image"><img src="images/pic06.jpg" alt="" /></a>
+                      <a href="#" class="image"><img src="http://www.runforever.co/images/pic06.jpg" alt="" /></a>
                     </article>
 
                   <!-- Mini Post -->
@@ -138,9 +138,9 @@
                       <header>
                         <h3><a href="#">Enim nisl veroeros</a></h3>
                         <time class="published" datetime="2015-10-17">October 17, 2015</time>
-                        <a href="#" class="author"><img src="images/avatar.jpg" alt="" /></a>
+                        <a href="#" class="author"><img src="http://www.runforever.co/images/avatar.jpg" alt="" /></a>
                       </header>
-                      <a href="#" class="image"><img src="images/pic07.jpg" alt="" /></a>
+                      <a href="#" class="image"><img src="http://www.runforever.co/images/pic07.jpg" alt="" /></a>
                     </article>
 
                 </div>
@@ -155,7 +155,7 @@
                         <h3><a href="#">Lorem ipsum fermentum ut nisl vitae</a></h3>
                         <time class="published" datetime="2015-10-20">October 20, 2015</time>
                       </header>
-                      <a href="#" class="image"><img src="images/pic08.jpg" alt="" /></a>
+                      <a href="#" class="image"><img src="http://www.runforever.co/images/pic08.jpg" alt="" /></a>
                     </article>
                   </li>
                   <li>
@@ -164,7 +164,7 @@
                         <h3><a href="#">Convallis maximus nisl mattis nunc id lorem</a></h3>
                         <time class="published" datetime="2015-10-15">October 15, 2015</time>
                       </header>
-                      <a href="#" class="image"><img src="images/pic09.jpg" alt="" /></a>
+                      <a href="#" class="image"><img src="http://www.runforever.co/images/pic09.jpg" alt="" /></a>
                     </article>
                   </li>
                   <li>
@@ -173,7 +173,7 @@
                         <h3><a href="#">Euismod amet placerat vivamus porttitor</a></h3>
                         <time class="published" datetime="2015-10-10">October 10, 2015</time>
                       </header>
-                      <a href="#" class="image"><img src="images/pic10.jpg" alt="" /></a>
+                      <a href="#" class="image"><img src="http://www.runforever.co/images/pic10.jpg" alt="" /></a>
                     </article>
                   </li>
                   <li>
@@ -182,7 +182,7 @@
                         <h3><a href="#">Magna enim accumsan tortor cursus ultricies</a></h3>
                         <time class="published" datetime="2015-10-08">October 8, 2015</time>
                       </header>
-                      <a href="#" class="image"><img src="images/pic11.jpg" alt="" /></a>
+                      <a href="#" class="image"><img src="http://www.runforever.co/images/pic11.jpg" alt="" /></a>
                     </article>
                   </li>
                   <li>
@@ -191,7 +191,7 @@
                         <h3><a href="#">Congue ullam corper lorem ipsum dolor</a></h3>
                         <time class="published" datetime="2015-10-06">October 7, 2015</time>
                       </header>
-                      <a href="#" class="image"><img src="images/pic12.jpg" alt="" /></a>
+                      <a href="#" class="image"><img src="http://www.runforever.co/images/pic12.jpg" alt="" /></a>
                     </article>
                   </li>
                 </ul>
