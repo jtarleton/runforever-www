fe5b831026f61927b0f9bc3bdae97e4b3880d8f7
James Tarletongit config --global user.name James <jamestarleton@gmail.com>
2016-01-31 10:20:50 -0500

    update reg form

--- lib/form/RfRegistrationForm.class.php
+++ lib/form/RfRegistrationForm.class.php
@@ -9,19 +9,17 @@ public function configure() {
 $this->disableCSRFProtection();
 
 $this->setWidgets(array(
-    'field1'    => new sfWidgetFormInputText(),
-    'field2'   => new sfWidgetFormInputText(array('default' => 'me@example.com')),
-    'subject' => new sfWidgetFormChoice(array('choices' => array('Subject A', 'Subject B', 'Subject C'))),
-    'message' => new sfWidgetFormTextarea(),
+    'username'    => new sfWidgetFormInputText(),
+    'email'   => new sfWidgetFormInputText(array('default' => 'me@example.com')),
+        'userpass'   => new sfWidgetFormInputText(array('default' => '')),
   ));
 
 
 $this->setValidators(
 	array(
-      		'field1' => new sfValidatorString(array('required' => true)), 
-      		'field2' => new sfValidatorString(array('required' => true)),
-		'subject'=>new sfValidatorPass,
-		'message'=>new sfValidatorPass
+      		'email' => new sfValidatorString(array('required' => true)), 
+      		'username' => new sfValidatorString(array('required' => true)),
+		      'userpass'=>new sfValidatorPass
     	)
 );
 
