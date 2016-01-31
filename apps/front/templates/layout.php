546acdfc57e6c0ccaa73cc2c0cf8a59f18e87a91
James Tarletongit config --global user.name James <jamestarleton@gmail.com>
2016-01-31 17:37:55 -0500

    update html

--- apps/front/templates/layout.php
+++ apps/front/templates/layout.php
@@ -1,7 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE HTML>
+<!--
+  Future Imperfect by Pixelarity
+  pixelarity.com @pixelarity
+  License: pixelarity.com/license
+-->
+<html>
   <head>
-    <?php include_http_metas() ?>
+    <title>Run Forever</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+    <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+      <?php include_http_metas() ?>
     <?php include_metas() ?>
     <?php include_title() ?>
 
@@ -17,7 +29,7 @@
     <meta name="author" content="">
     <link rel="shortcut icon" href="favicon.png">
     <title>Run Forever</title>
-    <link href="http://www.runforever.co/css/style.css" rel="stylesheet">
+   <!-- <link href="http://www.runforever.co/css/style.css" rel="stylesheet"> -->
 <link href="http://www.runforever.co/css/datatables.css" rel="stylesheet">
 
   <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
@@ -25,18 +37,11 @@
   
     <script src="js/chart.js"></script>
     <script type="text/javascript" src="js/datatables.js"></script>
-    <!--[if lt IE 9]><script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
 
   </head>
-
   <body>
-  <div class="sticky">
+
 <?php require('top.html'); ?>
 
 
@@ -44,10 +49,15 @@
 
 
 
-<br /><br />
 
 
-<br /><br />
+
+
+    <!-- Main -->
+          <div id="main">
+
+
+
 
 
 <?php if ($sf_user->hasFlash('notice')): ?>
@@ -58,30 +68,195 @@
   <div style="background-color:salmon; padding:10px;"  class="flash_error"><?php echo $sf_user->getFlash('error') ?></div>
 <?php endif ?>
 
-<br /><br />
-<br /><br />
+            
     <!-- Contant -->
 <?php echo $sf_content; ?>
 
-<!--     
-<img style="width:150px; height:150px;" src="http://www.hammernutrition.com/images/general/referral-discount.jpg"></img>
--->
 
 
-<div class="push">	</div>
-  </div>
-    <!--FOOTER-->
-    <div class="footer">
-      <div class="container clearfix">
-        <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>&copy; 2015 RunForever</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- Sidebar -->
+          <section id="sidebar">
+
+            <!-- Intro -->
+              <section id="intro">
+                <a href="#" class="logo"><img src="images/logo.jpg" alt="" /></a>
+                <header>
+                  <h2>Future Imperfect</h2>
+                  <p>Aliquet elit adipiscing nunc faucibus ac Vestibulum</p>
+                </header>
+              </section>
+
+            <!-- Mini Posts -->
+              <section>
+                <div class="mini-posts">
+
+                  <!-- Mini Post -->
+                    <article class="mini-post">
+                      <header>
+                        <h3><a href="#">Vitae sed condimentum</a></h3>
+                        <time class="published" datetime="2015-10-20">October 20, 2015</time>
+                        <a href="#" class="author"><img src="images/avatar.jpg" alt="" /></a>
+                      </header>
+                      <a href="#" class="image"><img src="images/pic04.jpg" alt="" /></a>
+                    </article>
+
+                  <!-- Mini Post -->
+                    <article class="mini-post">
+                      <header>
+                        <h3><a href="#">Rutrum neque accumsan</a></h3>
+                        <time class="published" datetime="2015-10-19">October 19, 2015</time>
+                        <a href="#" class="author"><img src="images/avatar.jpg" alt="" /></a>
+                      </header>
+                      <a href="#" class="image"><img src="images/pic05.jpg" alt="" /></a>
+                    </article>
+
+                  <!-- Mini Post -->
+                    <article class="mini-post">
+                      <header>
+                        <h3><a href="#">Odio congue mattis</a></h3>
+                        <time class="published" datetime="2015-10-18">October 18, 2015</time>
+                        <a href="#" class="author"><img src="images/avatar.jpg" alt="" /></a>
+                      </header>
+                      <a href="#" class="image"><img src="images/pic06.jpg" alt="" /></a>
+                    </article>
+
+                  <!-- Mini Post -->
+                    <article class="mini-post">
+                      <header>
+                        <h3><a href="#">Enim nisl veroeros</a></h3>
+                        <time class="published" datetime="2015-10-17">October 17, 2015</time>
+                        <a href="#" class="author"><img src="images/avatar.jpg" alt="" /></a>
+                      </header>
+                      <a href="#" class="image"><img src="images/pic07.jpg" alt="" /></a>
+                    </article>
+
+                </div>
+              </section>
+
+            <!-- Posts List -->
+              <section>
+                <ul class="posts">
+                  <li>
+                    <article>
+                      <header>
+                        <h3><a href="#">Lorem ipsum fermentum ut nisl vitae</a></h3>
+                        <time class="published" datetime="2015-10-20">October 20, 2015</time>
+                      </header>
+                      <a href="#" class="image"><img src="images/pic08.jpg" alt="" /></a>
+                    </article>
+                  </li>
+                  <li>
+                    <article>
+                      <header>
+                        <h3><a href="#">Convallis maximus nisl mattis nunc id lorem</a></h3>
+                        <time class="published" datetime="2015-10-15">October 15, 2015</time>
+                      </header>
+                      <a href="#" class="image"><img src="images/pic09.jpg" alt="" /></a>
+                    </article>
+                  </li>
+                  <li>
+                    <article>
+                      <header>
+                        <h3><a href="#">Euismod amet placerat vivamus porttitor</a></h3>
+                        <time class="published" datetime="2015-10-10">October 10, 2015</time>
+                      </header>
+                      <a href="#" class="image"><img src="images/pic10.jpg" alt="" /></a>
+                    </article>
+                  </li>
+                  <li>
+                    <article>
+                      <header>
+                        <h3><a href="#">Magna enim accumsan tortor cursus ultricies</a></h3>
+                        <time class="published" datetime="2015-10-08">October 8, 2015</time>
+                      </header>
+                      <a href="#" class="image"><img src="images/pic11.jpg" alt="" /></a>
+                    </article>
+                  </li>
+                  <li>
+                    <article>
+                      <header>
+                        <h3><a href="#">Congue ullam corper lorem ipsum dolor</a></h3>
+                        <time class="published" datetime="2015-10-06">October 7, 2015</time>
+                      </header>
+                      <a href="#" class="image"><img src="images/pic12.jpg" alt="" /></a>
+                    </article>
+                  </li>
+                </ul>
+              </section>
+
+            <!-- About -->
+              <section class="blurb">
+                <h2>About</h2>
+                <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod amet placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at phasellus sed ultricies.</p>
+                <ul class="actions">
+                  <li><a href="#" class="button">Learn More</a></li>
+                </ul>
+              </section>
+
+            <!-- Footer -->
+              <section id="footer">
+                <ul class="icons">
+                  <!-- <li><a href="#" class="fa-twitter"><span class="label">Twitter</span></a></li>
+                  <li><a href="#" class="fa-facebook"><span class="label">Facebook</span></a></li>
+                  <li><a href="#" class="fa-instagram"><span class="label">Instagram</span></a></li>
+                  <li><a href="#" class="fa-rss"><span class="label">RSS</span></a></li> -->
+                  <li><a href="#" class="fa-envelope"><span class="label">Email</span></a></li>
+                </ul>
+                <p class="copyright">&copy; Run Forever.</p>
+              </section>
+
+          </section>
+
       </div>
-    </div>
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <script src="js/bootstrap.js"></script>
+
+    <!-- Scripts -->
+      <script src="assets/js/jquery.min.js"></script>
+      <script src="assets/js/skel.min.js"></script>
+      <script src="assets/js/util.js"></script>
+      <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+      <script src="assets/js/main.js"></script>
+
   </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
