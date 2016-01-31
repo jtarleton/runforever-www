fa057882a3ca1614d1d52a4310d81fc56554f757
James Tarletongit config --global user.name James <jamestarleton@gmail.com>
2016-01-31 14:01:06 -0500

    update reg form

--- apps/front/modules/security/templates/invitedSuccess.php
+++ apps/front/modules/security/templates/invitedSuccess.php
@@ -1,1 +1,10 @@
-<a href="#" onclick="close_window();return false;">Close this</a> or to <a href="http://www.runforever.co">login</a>.
+<script type="text/javascript">
+function close_window() {
+  if (confirm("Close window?")) {
+    close();
+  }
+}
+
+</script>
+
+<a href="javascript:close_window();">Close this</a> or proceed to <a href="http://www.runforever.co">login</a>.
